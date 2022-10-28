dd4740f5c56f5675f3d1624d903fe912add037bb
alexthereaperlee <114201450+alexthereaperlee@users.noreply.github.com>
2022-10-28 11:55:21 +0800

    Add files via upload

---
+++ week08-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int i=1;
+    while(i<=n){
+        int k=1;
+    while(k<=n-i);
+        if(k<=n-i) printf(" ");
+        else printf("*");
+        k++;
+    }
+    printf("\n");
+    i++;
+}
